b5c2e3aeb258de25a61bb460aa78ca681e153fe3
i-Hmtl5 <i_lsy@sina.com>
2017-02-26 13:23:02 +0800

    Site updated: 2017-02-26 13:23:02

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#f00"
+        "value": "#f2849e"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#f00",
+        "color": "#f2849e",
         "opacity": 0.4,
         "width": 1
       },
